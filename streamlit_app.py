5ec431f6e1eabd7f1a785e8258e72fbc4063e27e
indwizkid <135019334+indwizkid@users.noreply.github.com>
2023-06-02 14:48:27 +0100

    Update streamlit_app.py
    previous commit reverted

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,25 +43,16 @@ except URLError as e:
 # write your own comment - what does this do?
 
 #streamlit.stop()
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 streamlit.stop()
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
-#my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from FRUIT_LOAD_LIST")
-    return my_cur.fetchall()
-#add a button to load the fruit
-if streamlit.button('get fruit load list'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+my_data_rows = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
 #streamlit.header("Hello from Snowflake:")
-#streamlit.dataframe(my_data_rows)
+streamlit.dataframe(my_data_rows)
 fruit_add_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
 #streamlit.write('The user entered ', fruit_add_choice)
 streamlit.text("Thanks for adding "+fruit_add_choice)
